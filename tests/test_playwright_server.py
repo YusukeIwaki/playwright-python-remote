94b397937b5fa22b2e3567ce7d5bc354cdef3296
YusukeIwaki <iwaki@i3-systems.com>
2021-09-05 21:19:53 +0900

    make test codes compatible with Playwright <= 1.12

--- tests/test_playwright_server.py
+++ tests/test_playwright_server.py
@@ -6,11 +6,14 @@ import os
 def test_connect():
     try:
         with sync_playwright_remote(os.environ['PLAYWRIGHT_SERVER_WS_ENDPOINT']) as playwright:
-            with playwright.chromium.launch() as browser:
+            browser = playwright.chromium.launch()
+            try:
                 page = browser.new_page(
                     viewport={'width': 1200, 'height': 800})
                 page.goto('https://github.com/')
                 page.screenshot(path="YusukeIwaki.png")
+            finally:
+                browser.close()
 
         png = Image.open('YusukeIwaki.png')
         assert png.width == 1200
